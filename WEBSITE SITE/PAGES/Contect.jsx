08fae7ccd988c7cabbdb9c645e3d73212f009dd0
Pdarshan4517 <149575945+Pdarshan4517@users.noreply.github.com>
2024-08-22 12:39:02 +0530

    Add files via upload

--- WEBSITE SITE/PAGES/Contect.jsx
+++ WEBSITE SITE/PAGES/Contect.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../COMMON/Header'
 import Footer from '../COMMON/Footer'
+import { toast } from 'react-toastify'
+import axios from 'axios'
 
 function Contect() {
+
+    const [formvalue , setformvalue]=useState({
+        name:"",
+        email:"",
+        message:"",
+        number:"",
+       
+    },[])
+
+    const changeHandel=(e)=>{
+        setformvalue(
+            {...formvalue, id: new Date().getMilliseconds().toString(),
+                [e.target.name]:e.target.value
+             });
+        console.log(formvalue);
+      }
+      function validation() {
+        var result = true;
+        if (formvalue.name === "") {
+          toast.error("Categories Name Field is required !");
+          result = false;
+          return false;
+        }
+        if (formvalue.email === "") {
+          toast.error("Categories email Field is required !");
+          result = false;
+          return false;
+        }
+        if (formvalue.message === "") {
+          toast.error("Categories message Field is required !");
+          result = false;
+          return false;
+        }
+       
+        if (formvalue.number === "") {
+          toast.error("Categories number Field is required !");
+          result = false;
+          return false;
+        }
+        return result;
+      }
+      const submitHandle = async(e)=>{
+        e.preventDefault();
+        if(validation()){
+          const res= await axios.post(`http://localhost:3000/contact`,formvalue);
+          setformvalue({...formvalue,name:"",email:"",message:"",number:""});
+          toast.success("Thanks for contacting us")
+        }        
+      }
     return (
         <div>
             <Header></Header>
@@ -41,38 +92,56 @@ function Contect() {
                         <div className="col-md-6">
                             <div className="wow fadeInUp" data-wow-delay="0.2s">
                                 <p className="mb-4">The contact form is currently inactive. Get a functional and working contact form with Ajax &amp; PHP in a few minutes. Just copy and paste the files, add a little code and you're done. <a href="https://htmlcodex.com/contact-form">Download Now</a>.</p>
-                                <form>
-                                    <div className="row g-3">
-                                        <div className="col-md-6">
-                                            <div className="form-floating">
-                                                <input type="text" className="form-control" id="name" placeholder="Your Name" />
-                                                <label htmlFor="name">Your Name</label>
-                                            </div>
-                                        </div>
-                                        <div className="col-md-6">
-                                            <div className="form-floating">
-                                                <input type="email" className="form-control" id="email" placeholder="Your Email" />
-                                                <label htmlFor="email">Your Email</label>
-                                            </div>
-                                        </div>
-                                        <div className="col-12">
-                                            <div className="form-floating">
-                                                <input type="text" className="form-control" id="subject" placeholder="Subject" />
-                                                <label htmlFor="subject">Subject</label>
-                                            </div>
-                                        </div>
-                                        <div className="col-12">
-                                            <div className="form-floating">
-                                                <textarea className="form-control" placeholder="Leave a message here" id="message" style={{ height: 100 }} defaultValue={""} />
-                                                <label htmlFor="message">Message</label>
-                                            </div>
-                                        </div>
-                                        <div className="col-12">
-                                            <button className="btn btn-primary w-100 py-3" type="submit">Send Message</button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
+                                <form id="request" className="main_form" mathod="post" onSubmit={submitHandle}>
+                                <div className="row">
+                                  <div className="col-md-6 ">
+                                    <input
+                                      className="form_control"
+                                      placeholder="Your name"
+                                      type="text"
+                                      value={formvalue.name}
+                                      onChange={changeHandel}
+                                      name="name"
+                                    />
+                                  </div>
+                                  <div className="col-md-6">
+                                    <input
+                                      className="form_control"
+                                      placeholder="email"
+                                      type="email"
+                                      value={formvalue.email}
+                                      onChange={changeHandel}
+                                      name="email"
+                                    />
+                                  </div>
+                                  <div className="col-md-6">
+                                    <input
+                                      className="form_control"
+                                      placeholder="number"
+                                      type="text" maxlength="10"
+                                      value={formvalue.number}
+                                      onChange={changeHandel}
+                                      name="number"
+                                    />
+                                  </div>
+                                 
+                                  <div className="col-md-12">
+                                    <textarea
+                                      style={{ color: "#d0d0cf" }}
+                                      className="textarea"
+                                      placeholder="Message"
+                                      type="type"
+                                      name="message"
+                                      value={formvalue.message}
+                                      onChange={changeHandel}
+                                      defaultValue={"message "}
+                                    />
+                                  </div>
+                                  <div className="col-md-12">
+                                    <button className="send_btn">Send Now</button>
+                                  </div>
+                                </div>
+                              </form>                         </div>
                         </div>
                     </div>
                 </div>
